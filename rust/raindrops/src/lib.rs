9c516a4984b851bddd4dc2a00f83a17f8b79ff21
Pablo Brasero <pablo@pablobm.com>
2016-06-07 23:10:58 +0100

    Completed: rust/raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+
+enum DropSound<'a> {
+    Name(&'a str),
+    Number,
+}
+
+use self::DropSound::*;
+
+impl<'a> fmt::Display for DropSound<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Name(name) => write!(f, "{}", name),
+            Number => write!(f, ""),
+        }
+    }
+}
+
+pub fn raindrops(n: i32) -> String {
+    let sounds = (
+        if n % 3 == 0 { Name("Pling") } else { Number },
+        if n % 5 == 0 { Name("Plang") } else { Number },
+        if n % 7 == 0 { Name("Plong") } else { Number },
+    );
+    match sounds {
+        (Number, Number, Number) => format!("{}", n),
+        (a, b, c)                => format!("{}{}{}", a, b, c),
+    }
+}

14e9510e289f48046feade739482b9114af04dd5
Pablo Brasero <pablo@pablobm.com>
2016-06-07 20:22:18 +0100

    Make it more readable

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_leap_year(year: i32) -> bool {
-    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    year.divisible_by(4) && (!year.divisible_by(100) || year.divisible_by(400))
+}
+
+trait DivisibleBy {
+    fn divisible_by(&self, divisor: i32) -> bool;
+}
+
+impl DivisibleBy for i32 {
+    fn divisible_by(&self, divisor: i32) -> bool {
+        self % divisor == 0
+    }
 }

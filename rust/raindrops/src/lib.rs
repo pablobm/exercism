b149f2c78ef7f784eadd45de074ff530c517eb99
Pablo Brasero <pablo@pablobm.com>
2016-06-08 19:08:30 +0100

    No need for enums. I can just compare strings

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,29 +1,13 @@
-use std::fmt;
-
-enum DropSound<'a> {
-    Name(&'a str),
-    Number,
-}
-
-use self::DropSound::*;
-
-impl<'a> fmt::Display for DropSound<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Name(name) => write!(f, "{}", name),
-            Number => write!(f, ""),
-        }
-    }
-}
-
 pub fn raindrops(n: i32) -> String {
     let sounds = (
-        if n % 3 == 0 { Name("Pling") } else { Number },
-        if n % 5 == 0 { Name("Plang") } else { Number },
-        if n % 7 == 0 { Name("Plong") } else { Number },
+        if n % 3 == 0 { "Pling" } else { "" },
+        if n % 5 == 0 { "Plang" } else { "" },
+        if n % 7 == 0 { "Plong" } else { "" },
     );
     match sounds {
-        (Number, Number, Number) => n.to_string(),
-        (a, b, c)                => format!("{}{}{}", a, b, c),
+        (a, b, c) if a == b && b == c && c == ""
+            => n.to_string(),
+        (a, b, c)
+            => format!("{}{}{}", a, b, c),
     }
 }

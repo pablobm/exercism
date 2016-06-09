134e15c125102938d6543f62b9006ce67de44980
Pablo Brasero <pablo@thoughtbot.com>
2016-06-09 12:34:33 +0100

    Completed: elixir/bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,10 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      String.strip(input) == "" -> "Fine. Be that way!"
+      String.last(input) == "?" -> "Sure."
+      input == String.upcase(input) && input != String.downcase(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
 end

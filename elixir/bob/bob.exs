294fea79332f64ded94d5c36f058383eb530bb9c
Pablo Brasero <pablo@pablobm.com>
2016-06-09 22:43:38 +0100

    Readable, with a bit of Elixir-ness

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,27 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.last(input) == "?" -> "Sure."
-      input == String.upcase(input) && input != String.downcase(input) -> "Whoa, chill out!"
-      true -> "Whatever."
+      silent?(input)
+        -> "Fine. Be that way!"
+      question?(input)
+        -> "Sure."
+      yelling?(input)
+        -> "Whoa, chill out!"
+      true
+        -> "Whatever."
     end
+  end
+
+  defp silent?(phrase) do
+    String.strip(phrase) == ""
+  end
+
+  defp question?(phrase) do
+    phrase |> String.ends_with?("?")
+  end
+
+  defp yelling?(phrase) do
+       phrase == String.upcase(phrase)
+    && phrase != String.downcase(phrase)
   end
 end

da4b78441fcd5efadafc39a641595b446d5329b4
Pablo Brasero <pablo@pablobm.com>
2016-06-11 22:17:01 +0100

    Functional

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -3,10 +3,9 @@ use std::collections::HashMap;
 static LATIN_ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
 
 pub fn is_pangram(phrase: &str) -> bool {
-    let mut phrase_letters = HashMap::new();
-    for ch in phrase.to_lowercase().chars() {
-        phrase_letters.insert(ch, true);
-    }
+    let phrase_letters = phrase.to_lowercase().chars()
+        .map(|x| (x, true))
+        .collect::<HashMap<_, _>>();
 
     LATIN_ALPHABET.chars()
         .all(|latin_letter| phrase_letters.contains_key(&latin_letter))

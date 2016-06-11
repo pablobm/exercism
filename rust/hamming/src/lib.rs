58dc610fc3ab7c2901f35a6708cc479b3f0cbd26
Pablo Brasero <pablo@pablobm.com>
2016-06-11 17:19:29 +0100

    Special case out of the way before main body

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn hamming_distance(s1: &str, s2: &str) -> Result<usize, &'static str> {
     if s1.len() != s2.len() {
-        Err("inputs of different length")
+        return Err("inputs of different length");
     }
-    else {
-        let distance = s1.chars().zip(s2.chars())
-            .filter(|&(n1, n2)| n1 != n2)
-            .count();
-        Ok(distance)
-    }
+
+    let distance = s1.chars().zip(s2.chars())
+        .filter(|&(n1, n2)| n1 != n2)
+        .count();
+    Ok(distance)
 }

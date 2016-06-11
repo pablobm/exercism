190babfbce306aa0594c24a6755c2b9936615c8a
Pablo Brasero <pablo@pablobm.com>
2016-06-11 22:19:33 +0100

    Could be used with other alphabets

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
-static LATIN_ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+static ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
 
 pub fn is_pangram(phrase: &str) -> bool {
     let phrase_letters = phrase.to_lowercase().chars()
         .map(|x| (x, true))
         .collect::<HashMap<_, _>>();
 
-    LATIN_ALPHABET.chars()
-        .all(|latin_letter| phrase_letters.contains_key(&latin_letter))
+    ALPHABET.chars()
+        .all(|alphabet_letter| phrase_letters.contains_key(&alphabet_letter))
 }

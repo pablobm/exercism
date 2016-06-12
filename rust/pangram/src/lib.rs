16511faf6f4879a43844cc8784fb89c75ac0736a
Pablo Brasero <pablo@pablobm.com>
2016-06-12 11:26:29 +0100

    Rust has implementations of sets

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,12 +1,11 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 static ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
 
 pub fn is_pangram(phrase: &str) -> bool {
     let phrase_letters = phrase.to_lowercase().chars()
-        .map(|x| (x, true))
-        .collect::<HashMap<_, _>>();
+        .collect::<HashSet<_>>();
 
     ALPHABET.chars()
-        .all(|alphabet_letter| phrase_letters.contains_key(&alphabet_letter))
+        .all(|alphabet_letter| phrase_letters.contains(&alphabet_letter))
 }

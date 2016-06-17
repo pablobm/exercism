5f96c47b960ba567ad9685c1c3e4c4e5141b1590
Pablo Brasero <pablo@thoughtbot.com>
2016-06-17 13:59:43 +0100

    Completed: elixir/word-count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,22 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    sentence
+    |> String.downcase
+    |> String.replace(~r/[^-\p{L}\p{N} ]+/u, " ")
+    |> String.split(" ")
+    |> Enum.filter(fn(word) -> word != "" end)
+    |> count_words
+  end
 
+  @spec count_words([String.t]) :: map
+  defp count_words([word | t]) do
+    counts = count_words(t)
+    word_count = counts[word] || 0
+    Map.put(counts, word, word_count + 1)
+  end
+
+  defp count_words([]) do
+    %{}
   end
 end

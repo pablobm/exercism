8cccfa7e351e0da935fbdbdf2727c0d24686cab4
Pablo Brasero <pablo@pablobm.com>
2016-06-19 19:48:08 +0100

    Functional!

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,18 +1,10 @@
 pub fn primes_up_to(limit: u32) -> Vec<u32> {
-    let mut primes = Vec::new();
+    (2..limit+1)
+        .filter(is_prime)
+        .collect()
+}
 
-    for potential_prime in 2..limit+1 {
-        let mut factor_found = false;
-        for potential_factor in 2..potential_prime {
-            if potential_prime % potential_factor == 0 {
-                factor_found = true;
-                break;
-            }
-        }
-        if ! factor_found {
-            primes.push(potential_prime);
-        }
-    }
-
-    primes
+fn is_prime(&num: &u32) -> bool {
+    (2..num)
+        .all(|x| num % x != 0)
 }

75adbc16ee2aeb320926abb0d885530cfb0c95f3
Pablo Brasero <pablo@pablobm.com>
2016-06-19 20:36:55 +0100

    More readable?

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -9,7 +9,16 @@ fn is_prime(&num: &u32) -> bool {
         return true;
     }
 
-    let limit = (num as f32).sqrt().ceil() as u32;
-    (2..limit+1)
+    (2..num.pseudo_sqrt()+1)
         .all(|x| num % x != 0)
+}
+
+trait Sqrt {
+    fn pseudo_sqrt(&self) -> u32;
+}
+
+impl Sqrt for u32 {
+    fn pseudo_sqrt(&self) -> u32 {
+        (*self as f32).sqrt().round() as u32
+    }
 }

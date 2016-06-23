003cf5a2ff312a6eaf135d71620090449ad7bdda
Pablo Brasero <pablo@pablobm.com>
2016-06-23 21:20:01 +0100

    DRY

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -19,81 +19,42 @@ impl ToString for Roman {
         let mut sections = LinkedList::new();
 
         let mut quotient = self.value;
-        sections.push_front(to_units_string(quotient % 10));
+        sections.push_front(roman_string(quotient % 10, "I", "V", "X"));
 
         quotient = quotient / 10;
         if quotient > 0 {
-            sections.push_front(to_tens_string(quotient % 10))
+            sections.push_front(roman_string(quotient % 10, "X", "L", "C"))
         }
 
         quotient = quotient / 10;
         if quotient > 0 {
-            sections.push_front(to_hundreds_string(quotient % 10))
+            sections.push_front(roman_string(quotient % 10, "C", "D", "M"))
         }
 
         quotient = quotient / 10;
         if quotient > 0 {
-            sections.push_front(to_thousands_string(quotient % 10))
+            sections.push_front(roman_string(quotient % 10, "M", "!", "!"))
         }
 
         sections.iter().map(|x| x.clone() ).collect()
     }
 }
 
-fn to_units_string(value: u16) -> String {
-    match value {
-        0 => "",
-        1 => "I",
-        2 => "II",
-        3 => "III",
-        4 => "IV",
-        5 => "V",
-        6 => "VI",
-        7 => "VII",
-        8 => "VIII",
-        9 => "IX",
-        _ => panic!("I don't know what to do with {}", value)
-    }.to_string()
-}
-
-fn to_tens_string(value: u16) -> String {
-    match value {
-        0 => "",
-        1 => "X",
-        2 => "XX",
-        3 => "XXX",
-        4 => "XL",
-        5 => "L",
-        6 => "LX",
-        7 => "LXX",
-        8 => "LXXX",
-        9 => "XC",
-        _ => panic!("I don't know what to do with {}", value)
-    }.to_string()
-}
-
-fn to_hundreds_string(value: u16) -> String {
-    match value {
-        0 => "",
-        1 => "C",
-        2 => "CC",
-        3 => "CCC",
-        4 => "CD",
-        5 => "D",
-        6 => "DC",
-        7 => "DCC",
-        8 => "DCCC",
-        9 => "CM",
-        _ => panic!("I don't know what to do with {}", value)
-    }.to_string()
-}
-
-fn to_thousands_string(value: u16) -> String {
-    match value {
-        0 => "",
-        1 => "M",
-        2 => "MM",
-        3 => "MMM",
-        _ => panic!("I don't know what to do with {}", value)
-    }.to_string()
+fn roman_string(digit: u16, one: &str, five: &str, ten: &str) -> String {
+    match digit {
+        0 => vec![],
+        1 => vec![one],
+        2 => vec![one, one],
+        3 => vec![one, one, one],
+        4 => vec![one, five],
+        5 => vec![five],
+        6 => vec![five, one],
+        7 => vec![five, one, one],
+        8 => vec![five, one, one, one],
+        9 => vec![one, ten],
+        _ => panic!("I don't know what to do with {}", digit),
+    }
+        .iter()
+        .map(|marks| marks.to_string())
+        .collect()
 }

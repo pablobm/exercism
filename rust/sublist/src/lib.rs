7ec609acd552f03e3d0a4acb9da03c2042493bb9
Pablo Brasero <pablo@pablobm.com>
2016-07-06 23:31:59 +0100

    Simpler

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -10,24 +10,13 @@ pub enum Comparison {
 pub fn sublist<T>(l1: &[T], l2: &[T]) -> Comparison
     where T: PartialEq
 {
-    if l1.len() == l2.len() {
-        if *l1 == *l2 {
-            Comparison::Equal
-        }
-        else {
-            Comparison::Unequal
-        }
+    if *l1 == *l2 {
+        Comparison::Equal
     }
-    else if l1.len() == 0 {
+    else if l1.is_empty() || l2.windows(l1.len()).any(|w| *w == *l1) {
         Comparison::Sublist
     }
-    else if l2.len() == 0 {
-        Comparison::Superlist
-    }
-    else if l1.len() < l2.len() && l2.windows(l1.len()).any(|w| *w == *l1) {
-        Comparison::Sublist
-    }
-    else if l1.len() > l2.len() && l1.windows(l2.len()).any(|w| *w == *l2) {
+    else if l2.is_empty() || l1.windows(l2.len()).any(|w| *w == *l2) {
         Comparison::Superlist
     }
     else {

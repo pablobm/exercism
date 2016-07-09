7b4ef55fdc053f501f2495e882c967164d8aa9af
Pablo Brasero <pablo@pablobm.com>
2016-07-10 00:21:01 +0100

    Completed: rust/robot-simulator

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -1,7 +1,6 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(PartialEq, Debug)]
+#[derive(Clone)]
+#[derive(Copy)]
 pub enum Direction {
     North,
     East,
@@ -9,36 +8,81 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+#[derive(Clone)]
+#[derive(Copy)]
+pub struct Robot {
+    x: isize,
+    y: isize,
+    d: Direction,
+}
+
+use Direction::*;
 
 impl Robot {
-    #[allow(unused_variables)]
+
     pub fn new(x: isize, y: isize, d: Direction) -> Self {
-        unimplemented!()
+        Robot {
+            x: x,
+            y: y,
+            d: d,
+        }
     }
 
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        let mut clone = self.clone();
+        clone.d = match clone.d {
+            North => East,
+            East  => South,
+            South => West,
+            West  => North,
+        };
+        clone
     }
 
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        let mut clone = self.clone();
+        clone.d = match clone.d {
+            North => West,
+            West  => South,
+            South => East,
+            East  => North,
+        };
+        clone
     }
 
     pub fn advance(self) -> Self {
-        unimplemented!()
+        let mut clone = self.clone();
+        match clone.d {
+            North => clone.y += 1,
+            East  => clone.x += 1,
+            South => clone.y -= 1,
+            West  => clone.x -= 1,
+        }
+        clone
     }
 
-    #[allow(unused_variables)]
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!()
+        let mut clone = self.clone();
+        for instruction in instructions.chars() {
+            clone = clone.apply_instruction(instruction);
+        }
+        clone
+    }
+
+    fn apply_instruction(self, instruction: char) -> Self {
+        match instruction {
+            'A' => self.advance(),
+            'R' => self.turn_right(),
+            'L' => self.turn_left(),
+            _   => panic!("I don't understand instruction `{}`", instruction),
+        }
     }
 
     pub fn position(&self) -> (isize, isize) {
-        unimplemented!()
+        (self.x, self.y)
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.d
     }
 }

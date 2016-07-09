9a3eb5c93e01e99233bca12262a80904397aed71
Pablo Brasero <pablo@pablobm.com>
2016-07-09 19:48:13 +0100

    DRY

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -7,14 +7,14 @@ pub struct Allergies {
 #[derive(Clone)]
 #[derive(Copy)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs         = 0b00000001,
+    Peanuts      = 0b00000010,
+    Shellfish    = 0b00000100,
+    Strawberries = 0b00001000,
+    Tomatoes     = 0b00010000,
+    Chocolate    = 0b00100000,
+    Pollen       = 0b01000000,
+    Cats         = 0b10000000,
 }
 
 use Allergen::*;
@@ -37,16 +37,7 @@ impl Allergies {
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        (self.bitmap & match allergen {
-            &Eggs         => 0b00000001,
-            &Peanuts      => 0b00000010,
-            &Shellfish    => 0b00000100,
-            &Strawberries => 0b00001000,
-            &Tomatoes     => 0b00010000,
-            &Chocolate    => 0b00100000,
-            &Pollen       => 0b01000000,
-            &Cats         => 0b10000000,
-        }) != 0
+        self.bitmap & *allergen as u8 != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {

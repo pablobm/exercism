a656cec777849bdd57fca30e4d33ff4b6c6daa18
Pablo Brasero <pablo@thoughtbot.com>
2016-07-18 19:21:23 +0100

    Better name

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,4 +1,4 @@
-RNA_COMPLEMENTS = {
+DNA_TO_RNA = {
     'C': 'G',
     'G': 'C',
     'T': 'A',
@@ -8,5 +8,5 @@ RNA_COMPLEMENTS = {
 def to_rna(nucleotides):
     transcription = []
     for n in nucleotides:
-        transcription.append(RNA_COMPLEMENTS[n])
+        transcription.append(DNA_TO_RNA[n])
     return ''.join(transcription)

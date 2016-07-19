058100387f7f442d55b94133da5888fbf6dde5cc
Pablo Brasero <pablo@thoughtbot.com>
2016-07-19 18:23:54 +0100

    Completed: elixir/anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    downcased_base = String.downcase(base)
+    candidates
+    |> Enum.reject(&(String.equivalent?(downcased_base, String.downcase(&1))))
+    |> Enum.filter(&(is_anagram?(downcased_base, &1)))
+  end
 
+  defp is_anagram?(word, candidate) do
+    ordered_letters(word) == ordered_letters(candidate)
+  end
+
+  defp ordered_letters(string) do
+    string
+    |> String.downcase
+    |> String.graphemes
+    |> Enum.sort
   end
 end

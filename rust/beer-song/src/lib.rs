d1a3cc0c7d0cbc16b2a825b874b0c2ae183d6d53
Pablo Brasero <pablo@thoughtbot.com>
2016-07-21 09:16:24 +0100

    This is way easier to read, and more efficient

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,8 +1,23 @@
 pub fn verse(initial: u32) -> String {
     match initial {
-         1 => verse_for( 1,  0),
-         0 => verse_for( 0, 99),
-         _ => verse_for(initial, initial-1),
+         2 =>
+             "2 bottles of beer on the wall, 2 bottles of beer.\n\
+              Take one down and pass it around, 1 bottle of beer on the wall.\n"
+                .to_string(),
+         1 =>
+             "1 bottle of beer on the wall, 1 bottle of beer.\n\
+              Take it down and pass it around, no more bottles of beer on the wall.\n"
+                .to_string(),
+         0 =>
+             "No more bottles of beer on the wall, no more bottles of beer.\n\
+              Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+                .to_string(),
+         _ =>
+             format!(
+                 "{} bottles of beer on the wall, {} bottles of beer.\n\
+                  Take one down and pass it around, {} bottles of beer on the wall.\n",
+                  initial, initial, initial-1
+             ),
     }
 }
 
@@ -12,41 +27,4 @@ pub fn sing(from_number: u32, to_number: u32) -> String {
         song = format!("{}\n{}", song, verse(number));
     }
     song
-}
-
-fn verse_for(initial: u32, next: u32) -> String {
-    let initial_phrase = phrase_for(initial);
-    let action = action_for(initial);
-    let next_phrase = phrase_for(next);
-    capitalize(&format!("{} of beer on the wall, {} of beer.\n\
-        {}, {} of beer on the wall.\n\
-        ", initial_phrase, initial_phrase, action, next_phrase))
-}
-
-fn phrase_for(number: u32) -> String {
-    match number {
-        0 => "no more bottles".to_string(),
-        1 => "1 bottle".to_string(),
-        _ => format!("{} bottles", number),
-    }
-}
-
-fn capitalize(sentence: &String) -> String {
-    let mut chars = sentence.chars();
-    let head = chars.next();
-
-    let capital = match head {
-        Some(c) => c.to_uppercase().next().unwrap().to_string(),
-        None    => "".to_string(),
-    };
-
-    format!("{}{}", capital, chars.collect::<String>())
-}
-
-fn action_for(number: u32) -> &'static str {
-    match number {
-        0 => "Go to the store and buy some more",
-        1 => "Take it down and pass it around",
-        _ => "Take one down and pass it around",
-    }
 }

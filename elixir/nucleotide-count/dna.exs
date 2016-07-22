7e817f077ea2c2653cd2ee479dcbb68403d52131
Pablo Brasero <pablo@thoughtbot.com>
2016-07-23 00:04:12 +0100

    Completed: elixir/nucleotide-count

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -13,8 +13,20 @@ defmodule DNA do
   1
   """
   @spec count([char], char) :: non_neg_integer
-  def count(strand, nucleotide) do
+  def count([], n) do
+    validate_nucleotide!(n)
+    0
+  end
 
+  def count([h | t], h) do
+    validate_nucleotide!(h)
+    1 + count(t, h)
+  end
+
+  def count([h | t], n) do
+    validate_nucleotide!(n)
+    validate_nucleotide!(h)
+    0 + count(t, n)
   end
 
 
@@ -28,6 +40,22 @@ defmodule DNA do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-
+    %{
+      ?A => count(strand, ?A),
+      ?T => count(strand, ?T),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G),
+    }
   end
+
+  defp validate_nucleotide!(n) do
+    case n do
+      ?A -> true
+      ?T -> true
+      ?C -> true
+      ?G -> true
+      _  -> raise ArgumentError
+    end
+  end
+
 end

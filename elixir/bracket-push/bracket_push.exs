64e7f305b7b3cae18708c71ee684f81ce8135bba
Pablo Brasero <pablo@thoughtbot.com>
2016-07-22 09:14:37 +0100

    Completed: elixir/bracket-push

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -4,6 +4,57 @@ defmodule BracketPush do
   """
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
+    check(String.graphemes(str), [])
+  end
 
+  def check([], []) do
+    true
+  end
+
+  def check([], _) do
+    false
+  end
+
+  def check([h | t1], [h | t2]) do
+    check(t1, t2)
+  end
+
+  def check([h | t], openings) do
+    cond do
+      is_opener?(h)
+        -> check(t, [closer_for(h) | openings])
+      is_closer?(h)
+        -> false
+      true
+        -> check(t, openings)
+    end
+  end
+
+  defp is_opener?(char) do
+    MapSet.member?(openers, char)
+  end
+
+  defp is_closer?(char) do
+    MapSet.member?(closers, char)
+  end
+
+  defp closer_for(opener) do
+    openers_to_closers[opener]
+  end
+
+  defp openers do
+    MapSet.new(Map.keys(openers_to_closers))
+  end
+
+  defp closers do
+    MapSet.new(Map.values(openers_to_closers))
+  end
+
+  defp openers_to_closers do
+    %{
+      "{" => "}",
+      "[" => "]",
+      "(" => ")",
+    }
   end
 end

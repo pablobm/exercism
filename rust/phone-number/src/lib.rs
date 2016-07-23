dbe9e8f7b05516e938257903409101ad7700ce89
Pablo Brasero <pablo@pablobm.com>
2016-07-23 23:27:57 +0100

    Get validity out of the way at the start

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,100 +1,68 @@
-use std::collections::LinkedList;
-
 struct PhoneNumber {
-    country: char,
-    area: LinkedList<char>,
-    exchange: LinkedList<char>,
-    subscriber: LinkedList<char>,
+    number: Option<String>,
+}
+
+fn sanitize(input: &str) -> String {
+    input.chars()
+        .filter(|c| c.is_numeric())
+        .collect()
+}
+
+fn parse(input: String) -> Option<String> {
+    if input.len() == 11 {
+        if input.starts_with("1") {
+            Some(input.chars().skip(1).take(10).collect())
+        }
+        else {
+            None
+        }
+    }
+    else if input.len() == 10 {
+        Some(input.to_string())
+    }
+    else {
+        None
+    }
 }
 
 impl PhoneNumber {
-    pub fn new(number: &str) -> PhoneNumber {
-        let mut tpl = PhoneNumber {
-            country: '1',
-            area: LinkedList::new(),
-            exchange: LinkedList::new(),
-            subscriber: LinkedList::new(),
-        };
-        let numbers = number.chars()
-            .filter(|n| n.is_numeric());
-
-        for (i, n) in numbers.rev().enumerate() {
-            match i {
-                0...3 => tpl.subscriber.push_front(n),
-                4...6 => tpl.exchange.push_front(n),
-                7...9 => tpl.area.push_front(n),
-                10    => tpl.country = n,
-                _     => {},
-            };
+    pub fn new(input: &str) -> PhoneNumber {
+        PhoneNumber {
+            number: parse(sanitize(input)),
         }
-
-        tpl
     }
 
-    pub fn is_valid(&self) -> bool {
-        self.area.len() == 3 &&
-            self.exchange.len() == 3 &&
-            self.subscriber.len() == 4 &&
-            self.country == '1'
+    pub fn area(&self) -> Option<String> {
+        self.number.clone().map(|n| n[0..3].to_string())
     }
 
-    pub fn area(&self) -> String {
-        stringify(&self.area)
+    pub fn exchange(&self) -> Option<String> {
+        self.number.clone().map(|n| n[3..6].to_string())
     }
 
-    pub fn exchange(&self) -> String {
-        stringify(&self.exchange)
+    pub fn subscriber(&self) -> Option<String> {
+        self.number.clone().map(|n| n[6..10].to_string())
     }
-
-    pub fn subscriber(&self) -> String {
-        stringify(&self.subscriber)
-    }
-}
-
-fn stringify(list: &LinkedList<char>) -> String {
-    list.iter().cloned().collect::<String>()
 }
 
 pub fn number(input: &str) -> Option<String> {
-    let phone_number = PhoneNumber::new(input);
-
-    if phone_number.is_valid() {
-        let string = format!(
-            "{}{}{}",
-            phone_number.area(),
-            phone_number.exchange(),
-            phone_number.subscriber(),
-        );
-        Some(string)
-    }
-    else {
-        None
-    }
+    PhoneNumber::new(input).number
 }
 
 pub fn area_code(input: &str) -> Option<String> {
-    let phone_number = PhoneNumber::new(input);
-
-    if phone_number.is_valid() {
-        Some(phone_number.area())
-    }
-    else {
-        None
-    }
+    PhoneNumber::new(input).area()
 }
 
 pub fn pretty_print(input: &str) -> String {
     let phone_number = PhoneNumber::new(input);
 
-    if phone_number.is_valid() {
-        format!(
+    match phone_number.number {
+        Some(_) => format!(
             "({}) {}-{}",
-            phone_number.area(),
-            phone_number.exchange(),
-            phone_number.subscriber(),
-        )
-    }
-    else {
-        "invalid".to_string()
+            phone_number.area().unwrap(),
+            phone_number.exchange().unwrap(),
+            phone_number.subscriber().unwrap(),
+        ),
+        None    => "invalid".to_string(),
     }
 }

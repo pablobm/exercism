6251cab045009cce2cb1046fd1d4f528e28c2a03
Pablo Brasero <pablo@thoughtbot.com>
2016-07-27 16:05:26 +0100

    Completed: elixir/hamming

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,6 +8,30 @@ defmodule DNA do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
+  def hamming_distance([], []) do
+    {:ok, 0}
+  end
+
+  def hamming_distance(_, []) do
+    error
+  end
+
+  def hamming_distance([], _) do
+    error
+  end
+
+  def hamming_distance([h | t1], [h | t2]) do
+    hamming_distance(t1, t2)
+  end
+
+  def hamming_distance([_ | t1], [_ | t2]) do
+    case hamming_distance(t1, t2) do
+      {:ok, distance} -> {:ok, distance + 1}
+      other -> other
+    end
+  end
+
+  defp error do
+    {:error, "Lists must be the same length."}
   end
 end
--- elixir/hamming/hamming_test.exs
+++ elixir/hamming/hamming_test.exs
@@ -12,27 +12,22 @@ defmodule DNATest do
     assert DNA.hamming_distance('', '') == {:ok, 0}
   end
 
-  @tag :pending
   test "no difference between identical strands" do
     assert DNA.hamming_distance('GGACTGA', 'GGACTGA') == {:ok, 0}
   end
 
-  @tag :pending
   test "small hamming distance in middle somewhere" do
     assert DNA.hamming_distance('GGACG', 'GGTCG') == {:ok, 1}
   end
 
-  @tag :pending
   test "distance with same nucleotides in different locations" do
     assert DNA.hamming_distance('TAG', 'GAT') == {:ok, 2}
   end
 
-  @tag :pending
   test "larger distance" do
     assert DNA.hamming_distance('ACCAGGG', 'ACTATGG') == {:ok, 2}
   end
 
-  @tag :pending
   test "hamming distance is undefined for strands of different lengths" do
     assert {:error, "Lists must be the same length."} = DNA.hamming_distance('AAAC', 'TAGGGGAGGCTAGCGGTAGGAC')
     assert {:error, "Lists must be the same length."} = DNA.hamming_distance('GACTACGGACAGGACACC', 'GACATCGC')

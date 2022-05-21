d13efcff5c6107daa4fa44c38bcb94da62006779
Pablo Brasero <pablo@pablobm.com>
2022-05-21 21:03:23 +0100

    Redownloaded exercise to get v2 metadata

--- rust/allergies/README.md
+++ rust/allergies/README.md
@@ -1,6 +1,11 @@
 # Allergies
 
-Write a program that, given a person's allergy score, can tell them whether or not they're allergic to a given item, and their full list of allergies.
+Welcome to Allergies on Exercism's Rust Track.
+If you need help running the tests or submitting your code, check out `HELP.md`.
+
+## Instructions
+
+Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies.
 
 An allergy test produces a single numeric score which contains the
 information about all the allergies the person has (that they were
@@ -24,37 +29,38 @@ Now, given just that score of 34, your program should be able to say:
 - Whether Tom is allergic to any one of those allergens listed above.
 - All the allergens Tom is allergic to.
 
-## Rust Installation
-
-Refer to the [exercism help page][help-page] for Rust installation and learning
-resources.
-
-## Writing the Code
-
-Execute the tests with:
-
-```bash
-$ cargo test
-```
-
-All but the first test have been ignored.  After you get the first test to
-pass, remove the ignore flag (`#[ignore]`) from the next test and get the tests
-to pass again.  The test file is located in the `tests` directory.   You can
-also remove the ignore flag from all the tests to get them to run all at once
-if you wish.
-
-Make sure to read the [Crates and Modules](https://doc.rust-lang.org/stable/book/crates-and-modules.html) chapter if you
-haven't already, it will help you with organizing your files.
-
-## Feedback, Issues, Pull Requests
-
-The [exercism/xrust](https://github.com/exercism/xrust) repository on GitHub is the home for all of the Rust exercises. If you have feedback about an exercise, or want to help implement new exercises, head over there and create an issue. Members of the [rust track team](https://github.com/orgs/exercism/teams/rust) are happy to help!
-
-If you want to know more about Exercism, take a look at the [contribution guide](https://github.com/exercism/x-common/blob/master/CONTRIBUTING.md).
-
-[help-page]: http://exercism.io/languages/rust
-[crates-and-modules]: http://doc.rust-lang.org/stable/book/crates-and-modules.html
+Note: a given score may include allergens **not** listed above (i.e.
+allergens that score 256, 512, 1024, etc.).  Your program should
+ignore those components of the score.  For example, if the allergy
+score is 257, your program should only report the eggs (1) allergy.
 
 ## Source
 
-Jumpstart Lab Warm-up [http://jumpstartlab.com](http://jumpstartlab.com)
+### Created by
+
+- @EduardoBautista
+
+### Contributed to by
+
+- @ashleygwilliams
+- @coriolinus
+- @cwhakes
+- @EduardoBautista
+- @efx
+- @ErikSchierboom
+- @IanWhitney
+- @kytrinyx
+- @lutostag
+- @mkantor
+- @navossoc
+- @nfiles
+- @petertseng
+- @rofrol
+- @stringparser
+- @taravancil
+- @xakon
+- @ZapAnton
+
+### Based on
+
+Jumpstart Lab Warm-up - http://jumpstartlab.com
--- rust/allergies/tests/allergies.rs
+++ rust/allergies/tests/allergies.rs
@@ -1,129 +1,135 @@
-extern crate allergies;
-
 use allergies::*;
 
-fn compare_allergy_vectors(expected: &Vec<Allergen>, actual: &Vec<Allergen>) {
+fn compare_allergy_vectors(expected: &[Allergen], actual: &[Allergen]) {
     for element in expected {
         if !actual.contains(element) {
-            panic!("Allergen missing\n  {:?} should be in {:?}",
-                   element,
-                   actual);
+            panic!(
+                "Allergen missing\n  {:?} should be in {:?}",
+                element, actual
+            );
         }
     }
 
     if actual.len() != expected.len() {
-        panic!("Allergy vectors are of different lengths\n  expected {:?}\n  got {:?}",
-               expected,
-               actual);
+        panic!(
+            "Allergy vectors are of different lengths\n  expected {:?}\n  got {:?}",
+            expected, actual
+        );
     }
 }
 
 #[test]
 fn is_not_allergic_to_anything() {
     let allergies = Allergies::new(0);
-    assert_eq!(false, allergies.is_allergic_to(&Allergen::Peanuts));
-    assert_eq!(false, allergies.is_allergic_to(&Allergen::Cats));
-    assert_eq!(false, allergies.is_allergic_to(&Allergen::Strawberries));
+    assert!(!allergies.is_allergic_to(&Allergen::Peanuts));
+    assert!(!allergies.is_allergic_to(&Allergen::Cats));
+    assert!(!allergies.is_allergic_to(&Allergen::Strawberries));
 }
 
 #[test]
 fn is_allergic_to_eggs() {
-    assert_eq!(true, Allergies::new(1).is_allergic_to(&Allergen::Eggs));
+    assert!(Allergies::new(1).is_allergic_to(&Allergen::Eggs));
 }
 
 #[test]
 fn is_allergic_to_egg_shellfish_and_strawberries() {
     let allergies = Allergies::new(5);
-    assert_eq!(true, allergies.is_allergic_to(&Allergen::Eggs));
-    assert_eq!(true, allergies.is_allergic_to(&Allergen::Shellfish));
-    assert_eq!(false, allergies.is_allergic_to(&Allergen::Strawberries));
+    assert!(allergies.is_allergic_to(&Allergen::Eggs));
+    assert!(allergies.is_allergic_to(&Allergen::Shellfish));
+    assert!(!allergies.is_allergic_to(&Allergen::Strawberries));
 }
 
 #[test]
 fn no_allergies_at_all() {
-    let expected: Vec<Allergen> = Vec::new();
+    let expected = &[];
     let allergies = Allergies::new(0).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_just_eggs() {
-    let expected = vec![Allergen::Eggs];
+    let expected = &[Allergen::Eggs];
     let allergies = Allergies::new(1).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_just_peanuts() {
-    let expected = vec![Allergen::Peanuts];
+    let expected = &[Allergen::Peanuts];
     let allergies = Allergies::new(2).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_just_strawberries() {
-    let expected = vec![Allergen::Strawberries];
+    let expected = &[Allergen::Strawberries];
     let allergies = Allergies::new(8).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_eggs_and_peanuts() {
-    let expected = vec![Allergen::Eggs, Allergen::Peanuts];
+    let expected = &[Allergen::Eggs, Allergen::Peanuts];
     let allergies = Allergies::new(3).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_eggs_and_shellfish() {
-    let expected = vec![Allergen::Eggs, Allergen::Shellfish];
+    let expected = &[Allergen::Eggs, Allergen::Shellfish];
     let allergies = Allergies::new(5).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_many_things() {
-    let expected = vec![Allergen::Strawberries,
-                        Allergen::Tomatoes,
-                        Allergen::Chocolate,
-                        Allergen::Pollen,
-                        Allergen::Cats];
+    let expected = &[
+        Allergen::Strawberries,
+        Allergen::Tomatoes,
+        Allergen::Chocolate,
+        Allergen::Pollen,
+        Allergen::Cats,
+    ];
     let allergies = Allergies::new(248).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn allergic_to_everything() {
-    let expected = vec![Allergen::Eggs,
-                        Allergen::Peanuts,
-                        Allergen::Shellfish,
-                        Allergen::Strawberries,
-                        Allergen::Tomatoes,
-                        Allergen::Chocolate,
-                        Allergen::Pollen,
-                        Allergen::Cats];
+    let expected = &[
+        Allergen::Eggs,
+        Allergen::Peanuts,
+        Allergen::Shellfish,
+        Allergen::Strawberries,
+        Allergen::Tomatoes,
+        Allergen::Chocolate,
+        Allergen::Pollen,
+        Allergen::Cats,
+    ];
     let allergies = Allergies::new(255).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }
 
 #[test]
 fn scores_over_255_do_not_trigger_false_positives() {
-    let expected = vec![Allergen::Eggs,
-                        Allergen::Shellfish,
-                        Allergen::Strawberries,
-                        Allergen::Tomatoes,
-                        Allergen::Chocolate,
-                        Allergen::Pollen,
-                        Allergen::Cats];
+    let expected = &[
+        Allergen::Eggs,
+        Allergen::Shellfish,
+        Allergen::Strawberries,
+        Allergen::Tomatoes,
+        Allergen::Chocolate,
+        Allergen::Pollen,
+        Allergen::Cats,
+    ];
     let allergies = Allergies::new(509).allergies();
 
-    compare_allergy_vectors(&expected, &allergies);
+    compare_allergy_vectors(expected, &allergies);
 }

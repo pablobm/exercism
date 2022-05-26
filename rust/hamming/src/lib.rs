c822be0d93ba57d9ccf22247c04f8508b8af6336
Pablo Brasero <pablo@pablobm.com>
2022-05-26 07:18:11 +0100

    Fixed to work with latest version of exercise

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,10 +1,10 @@
-pub fn hamming_distance(s1: &str, s2: &str) -> Result<usize, &'static str> {
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     if s1.len() != s2.len() {
-        return Err("inputs of different length");
+        return None;
     }
 
     let distance = s1.chars().zip(s2.chars())
         .filter(|&(n1, n2)| n1 != n2)
         .count();
-    Ok(distance)
+    Some(distance)
 }

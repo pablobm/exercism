e2695ca776ec8ceefc57b2d9bfcdca8d555b995d
Pablo Brasero <pablo@pablobm.com>
2022-05-26 07:31:16 +0100

    Fixed to work with latest version of exercise

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,13 +1,13 @@
-pub fn is_leap_year(year: i32) -> bool {
+pub fn is_leap_year(year: u64) -> bool {
     year.divisible_by(4) && (!year.divisible_by(100) || year.divisible_by(400))
 }
 
 trait DivisibleBy {
-    fn divisible_by(&self, divisor: i32) -> bool;
+    fn divisible_by(&self, divisor: u64) -> bool;
 }
 
-impl DivisibleBy for i32 {
-    fn divisible_by(&self, divisor: i32) -> bool {
+impl DivisibleBy for u64 {
+    fn divisible_by(&self, divisor: u64) -> bool {
         self % divisor == 0
     }
 }

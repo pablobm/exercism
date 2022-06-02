c64ed61bbb025c8b2bf2670c864b08c26f31503a
Pablo Brasero <pablo@pablobm.com>
2022-06-02 14:10:38 +0100

    Fixed to work with latest version of exercise

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,14 +1,38 @@
 use std::collections::HashMap;
 
-pub fn count(nucleotide: char, sequence: &str) -> usize {
-    sequence.chars().filter(|&ch| ch == nucleotide).count()
+const VALID_NUCLEOTIDES: &str = "ATGC";
+
+pub fn count(nucleotide: char, sequence: &str) -> Result<usize, char> {
+    if !VALID_NUCLEOTIDES.contains(nucleotide) {
+        return Err(nucleotide)
+    }
+
+    let mut count = 0;
+
+    for c in sequence.chars() {
+        if !VALID_NUCLEOTIDES.contains(c) {
+            return Err(c)
+        }
+        if c == nucleotide {
+            count += 1;
+        }
+    }
+    Ok(count)
 }
 
-pub fn nucleotide_counts(sequence: &str) -> HashMap<char, usize> {
+pub fn nucleotide_counts(sequence: &str) -> Result<HashMap<char, usize>, char> {
     let mut map = HashMap::new();
-    map.insert('A', count('A', sequence));
-    map.insert('T', count('T', sequence));
-    map.insert('G', count('G', sequence));
-    map.insert('C', count('C', sequence));
-    map
+    for nuc in VALID_NUCLEOTIDES.chars() {
+        let result = count(nuc, sequence);
+        match result {
+            Ok(number) => {
+                map.insert(nuc, number);
+                ()
+            }
+            Err(err) => {
+                return Err(err)
+            }
+        }
+    }
+    Ok(map)
 }

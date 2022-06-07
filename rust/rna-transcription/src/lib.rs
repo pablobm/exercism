0204c4e33da0cf51f9df0c426f09d28664945f82
Pablo Brasero <pablo@pablobm.com>
2022-06-07 22:08:03 +0100

    Fixed to work with latest version of exercise

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,35 +1,71 @@
 #[derive(PartialEq)]
 #[derive(Debug)]
-pub struct RibonucleicAcid {
+pub struct Rna {
     sequence: String,
 }
 
-impl RibonucleicAcid {
-    pub fn new(sequence: &str) -> RibonucleicAcid {
-        RibonucleicAcid {
-            sequence: sequence.to_string(),
+fn is_not_valid_rna_nucleotide(nucleotide: char) -> bool {
+    !is_valid_rna_nucleotide(nucleotide)
+}
+
+fn is_not_valid_dna_nucleotide(nucleotide: char) -> bool {
+    !is_valid_dna_nucleotide(nucleotide)
+}
+
+fn is_valid_rna_nucleotide(nucleotide: char) -> bool {
+    match nucleotide {
+        'A' | 'U' | 'G' | 'C' => true,
+        _ => false
+    }
+}
+
+fn is_valid_dna_nucleotide(nucleotide: char) -> bool {
+    match nucleotide {
+        'A' | 'T' | 'G' | 'C' => true,
+        _ => false
+    }
+}
+
+impl Rna {
+    pub fn new(sequence: &str) -> Result<Rna, usize> {
+        let maybe_index = sequence.find(is_not_valid_rna_nucleotide);
+        match maybe_index {
+            None => (),
+            Some(index) => return Err(index)
         }
+
+        let rna = Rna {
+            sequence: sequence.to_string(),
+        };
+        Ok(rna)
     }
 }
 
 #[derive(PartialEq)]
 #[derive(Debug)]
-pub struct DeoxyribonucleicAcid {
+pub struct Dna {
     sequence: String,
 }
 
-impl DeoxyribonucleicAcid {
-    pub fn new(sequence: &str) -> DeoxyribonucleicAcid {
-        DeoxyribonucleicAcid {
-            sequence: sequence.to_string(),
+impl Dna {
+    pub fn new(sequence: &str) -> Result<Dna, usize> {
+        let maybe_index = sequence.find(is_not_valid_dna_nucleotide);
+        match maybe_index {
+            None => (),
+            Some(index) => return Err(index)
         }
+
+        let dna = Dna {
+            sequence: sequence.to_string(),
+        };
+        Ok(dna)
     }
 
-    pub fn to_rna(&self) -> RibonucleicAcid {
+    pub fn into_rna(&self) -> Rna {
         let rna_sequence = self.sequence.chars()
             .map(transcribe)
             .collect::<String>();
-        RibonucleicAcid::new(&rna_sequence)
+        Rna::new(&rna_sequence).unwrap()
     }
 }
 

3b122b384aee5c9f4d619648ea67f45d228e90c2
Pablo Brasero <pablo@pablobm.com>
2022-06-08 20:51:35 +0100

    Fixed to work with latest version of exercise

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+const SINGLE_QUOTE : char = '\'';
+
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
     let mut counts = HashMap::new();
     let words = phrase
@@ -7,6 +9,7 @@ pub fn word_count(phrase: &str) -> HashMap<String, u32> {
         .split(is_separator)
         .filter(|word| !word.is_empty() )
         .map(|word| word.to_string())
+        .map(remove_quotations)
         .collect::<Vec<String>>();
 
     for word in words {
@@ -17,8 +20,21 @@ pub fn word_count(phrase: &str) -> HashMap<String, u32> {
     counts
 }
 
+fn remove_quotations(s: String) -> String {
+    if s.len() < 2 {
+        return s
+    }
+
+    let mut each_char = s.chars();
+
+    if each_char.next() == Some(SINGLE_QUOTE) && each_char.last() == Some(SINGLE_QUOTE) {
+        return s[1..=s.len()-2].to_string()
+    }
+    s
+}
+
 fn is_word_character(ch: char) -> bool {
-    ch.is_alphanumeric()
+    ch.is_alphanumeric() || ch == SINGLE_QUOTE
 }
 
 fn is_separator(ch: char) -> bool {
